f48f9c23a983c1c61dcbc6bc588e2af5215f4dfb
clonalejandro <contact@clonalejandro.me>
2017-04-24 02:10:54 +0200

    Base 0.1.2

--- src/main/java/me/clonalejandro/AmazingHats/Main.java
+++ src/main/java/me/clonalejandro/AmazingHats/Main.java
@@ -1,5 +1,10 @@
 package me.clonalejandro.AmazingHats;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by alejandrorioscalera
  * On 24/4/17
@@ -16,5 +21,70 @@ package me.clonalejandro.AmazingHats;
  * All rights reserved for clonalejandro ©AmazingHats 2017 / 2018
  */
 
-public class Main {
+public class Main extends JavaPlugin {
+
+
+    /** SMALL CONSTRUCTORS **/
+
+    public static Main instance;
+
+
+    /** REST **/
+
+    @Override
+    public void onLoad(){
+        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "AmazingHats> " + ChatColor.BLUE + "plugin loaded");
+    }
+
+
+    @Override
+    public void onEnable(){
+
+        try {
+
+            instance = this;
+
+            Config();
+            Events();
+            Commands();
+
+            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "AmazingHats> " + ChatColor.GREEN + "plugin activated");
+        } catch (Exception ex){
+
+            ex.printStackTrace();
+            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "AmazingHats> " + ChatColor.DARK_RED + "several error");
+
+            instance = null;
+        }
+    }
+
+
+    @Override
+    public void onDisable(){
+        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "AmazingHats> " + ChatColor.RED + "plugin disabled");
+        instance = null;
+    }
+
+
+    /** REST **/
+
+    public void Config(){
+        saveDefaultConfig();
+    }
+
+
+    public void Events(){
+        PluginManager pm = instance.getServer().getPluginManager();
+        //TODO: Listeners
+    }
+
+
+    public void Commands(){
+        //TODO: Commands
+    }
+
+
+    /** OTHERS **/
+
+
 }
