27da35b21ffb971abd9e8e3067451147c3140091
clonalejandro <contact@clonalejandro.me>
2017-08-16 13:43:34 +0200

    Implement listeners of skull gui

---
+++ src/main/java/me/clonalejandro/AmazingHats/listeners/HatskullListener.java
@@ -0,0 +1,87 @@
+package me.clonalejandro.AmazingHats.listeners;
+
+import me.clonalejandro.AmazingHats.Main;
+import me.clonalejandro.AmazingHats.gui.HatGui;
+import me.clonalejandro.AmazingHats.hats.heads.HeadHat;
+import me.clonalejandro.AmazingHats.utils.HatManager;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Created by alejandrorioscalera
+ * On 14/8/17
+ *
+ * -- SOCIAL NETWORKS --
+ *
+ * GitHub: https://github.com/clonalejandro or @clonalejandro
+ * Website: https://clonalejandro.me/
+ * Twitter: https://twitter.com/clonalejandro11/ or @clonalejandro11
+ * Keybase: https://keybase.io/clonalejandro/
+ *
+ * -- LICENSE --
+ *
+ * All rights reserved for clonalejandro ©AmazingHats 2017 / 2018
+ */
+
+public class HatskullListener implements Listener {
+
+
+    /** SMALL CONSTRUCTORS **/
+
+    private final Main plugin;
+
+    public HatskullListener(Main instance){
+        plugin = instance;
+    }
+
+    private boolean cancelled = false;
+
+
+    /** REST **/
+
+    /**
+     * @param e
+     * @return
+     */
+    private ItemStack getItem(InventoryClickEvent e){
+        return e.getCurrentItem();
+    }
+
+
+    /** OTHERS **/
+
+    @EventHandler
+    public void SkullHatInventory(InventoryClickEvent e){
+
+        Player p = (Player) e.getWhoClicked();
+
+        HatGui hatGui = new HatGui(plugin);
+
+        String name = hatGui.serial;
+
+        name = ChatColor.translateAlternateColorCodes('&', name);
+
+        if (e.getClickedInventory().getName().equalsIgnoreCase(name)){
+            ItemStack item = getItem(e);
+
+            ifs(item, p);
+
+            if (cancelled) e.setCancelled(true);
+            else e.setCancelled(false);
+        }
+    }
+
+
+    private void ifs(ItemStack item, Player p){
+
+        if (item.getItemMeta().getDisplayName().contains("Notch")){
+            HatManager hatManager = new HatManager(plugin);
+            hatManager.setSkullHat(p, HeadHat.Notch);
+            cancelled = true;
+        }
+    }
+}
